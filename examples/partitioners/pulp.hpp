02082deb08078ba9696a230afd6a240c60f83b83
Jan-Willem Buurlage <janwillem@buurlagewits.nl>
2015-09-15 13:45:07 +0000

    (squash) Work on PuLP

--- examples/partitioners/pulp.hpp
+++ examples/partitioners/pulp.hpp
@@ -28,50 +28,40 @@ class PulpPartitioner : Zee::IterativePartitioner<TMatrix>
         }
 
         virtual TMatrix& refineWithIterations(TIdx iters) {
-
             auto& images = A.getMutableImages();
-            for(auto& image : images) {
-                auto nz = image->nonZeros();
-                if (nz == 0) {
-                    cerr << "ERROR: Matrix image empty!" << endl;
-                    continue;
+            auto p = A.getProcs();
+
+            void distCs = [] (
+                std::shared_ptr<typename TMatrix::image_type> _img,
+                TIdx s)
+            {
+                for (auto& pRowCount : img->getRowSet()) {
+                    auto row = pRowCount.first;
+                    auto count = pRowCount.second;
+                    
+                    // TODO Fixme syntax how
+                    Zee::message::put(s, row % p);
+                    Zee::message::put(row, row % p);
+                    Zee::message::put(count, row % p);
                 }
 
-                for (auto& trip : *image) {
-                    // actually want to run this simultaeneously for each procs
-                    // think
-
-                    // put in some kind of request
-                    auto countLambda = [col_ = col, row_ = row]
-                        (std::shared_ptr<typename TMatrix::image_type> _img)
-                        -> typename TMatrix::index_type
-                    {
-                        typename TMatrix::index_type count = 0;
-                        for (auto& a : *_img) {
-                            if (a.col() == col_ || a.row() == row_)
-                                count++;
-                        }
-                        return count;
-                    };
-
-                    auto counts = A.template compute<
-                        typename TMatrix::index_type>(countLambda);
-
-                    auto max = 0;
-                    auto max_index = -1;
-                    for (typename TMatrix::index_type i = 0;
-                            i < counts.size(); ++i) {
-                        if (counts[i] > max) {
-                            max = counts[i];
-                            max_index = i;
-                        }
-                    }
-
-                    // give element to max_index
-                    image->popElement(element_index);
-                    images[max_index]->pushTriplet(trip);
+                for (auto& pColCount : img->getColSet()) {
+                    auto col = pColCount.first;
+                    auto count = pColCount.second;
+                   
+                    // TODO Fixme
+                    Zee::message::put(s, col % p);
+                    Zee::message::put(row, col % p);
+                    Zee::message::put(count, col % p);
                 }
-            }
+            };
+
+            void redistCs = [] (
+                std::shared_ptr<typename TMatrix::image_type> _img,
+                TIdx s)
+            {
+
+            };
         }
 
         void TMatrix& refine() override
