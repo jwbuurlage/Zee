f6b4d7918089b229db75c56994647fd3bdcfdee6
Jan-Willem Buurlage <janwillembuurlage@gmail.com>
2015-10-08 11:12:41 +0200

    Add layout for loading test

---
+++ test/loading_matrices.cpp
@@ -0,0 +1,24 @@
+
+
+#include "catch.hpp"
+
+#include <zee.hpp>
+
+using TIdx = uint32_t;
+using TVal = float;
+
+TEST_CASE("load random matrix", "[loading matrices]") {
+    REQUIRE(0 == 0);
+}
+
+TEST_CASE("load identity matrix", "[loading matrices]") {
+    REQUIRE(0 == 0);
+}
+
+TEST_CASE("load matrix market", "[loading matrices]") {
+    REQUIRE(0 == 0);
+}
+
+TEST_CASE("explicitely set elements", "[loading matrices]") {
+    REQUIRE(0 == 0);
+}
